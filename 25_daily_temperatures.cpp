5e5cf5ee56dc7373ffb37c9a82b030f4920f59fb
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-09-05 22:44:04 +0900

    added comments on explaining the algorithm

--- 25_daily_temperatures.cpp
+++ 25_daily_temperatures.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+
+	// dailyTemperatures
+	// create a stack that holds a pair of values: (temperature, index)
+	// create a ret vector (for the answer) of the same size as the temperatures
+	// 		vector and initialize the values to 0.
+	// push to the stack, (temp[0], 0) since this is the same no matter what.
+	//
+	// loop over the temperatures vector (obviously starting at idx 1),
+	// since we want to keep a monotonic decreasing stack,
+	//
+	// 		check whether the top of the stack is less than the current temp.
+	// 		this means the temperature has increased, 
+	// 		and the value of ret at this idx should be updated.
+	// 		Since we want the number of days you have to wait until a temp increase,
+	// 		ret[idx of the top of the stack] = i - idx of the top of the stack.
+	// 		now pop the stack.
+	// 		repeat until the top of stack is not less than the current temp
+	//
+	// 		push current temp and idx to stack
+	// end of loop, return ret.
     vector<int> dailyTemperatures(vector<int>& temperatures) {
 		vector<int> ret(temperatures.size(), 0);
 
