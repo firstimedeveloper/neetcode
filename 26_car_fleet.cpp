a59dd2239066fbf47e8203d77a5ef72ee758ccd8
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-09-06 18:25:38 +0900

    added comments that explain the algo

--- 26_car_fleet.cpp
+++ 26_car_fleet.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
+	// turns
+	// calculates the number of turns it will take for the car to reach target
+	// given its position and speed.
 	float turns(int target, pair<int, int> car) {
 		return (target - car.first) / (float) car.second;
 	}
+	// Algo:
+	// We want to create a new vector that holds the car's info (pos,speed), sorted asc.
+	// Since the cars cannot pass each other, the order of the cars will always be the same.
+	// We want to loop the new vector backwards.
+	// We have a stack that keeps track of the number of fleets.
+	// This stack need not hold all the cars of a given fleet, only its front-most car.
+	// Inside the loop, we add the car if the car will never meet the car at the top of the stack.
+	// If the current car never meets the car at the top of the stack, that means, the current car is the front-most car of a fleet.
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
 		vector<pair<int, int>> cars;
 		stack<pair<int, int>> s;
