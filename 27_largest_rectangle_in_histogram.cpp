d0c2bfb6e15ff0db9b13d9f5c6c6dbe97e1ef7f2
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-09-07 17:17:17 +0900

    added comments

--- 27_largest_rectangle_in_histogram.cpp
+++ 27_largest_rectangle_in_histogram.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+	// algorithm
+	// create a stack that contains a pair(height, pos)
+	// The stack will be monotonically increasing.
+	// while looping through the bars, we check if the current bar's height is 
+	// smaller than the top of the stack.
+	// if so, we pop the bars in the stack until the above is not true.
+	// while popping, we recalculate the max area. 
+	// also, we keep track of the idx of the popped bar, since the cur bar's idx should be the idx of the last popped bar.
+	//
     int largestRectangleArea(vector<int>& heights) {
 		int ret = 0;
 		stack<pair<int, int>> s; // (height, pos)
@@ -7,7 +16,7 @@ public:
             int start = i;
             while (!s.empty() && heights[i] < s.top().first) {
                 ret = max(ret, (i - s.top().second) * s.top().first);
-                start = s.top().second;
+                start = s.top().second; // important. the start becomes the idx of the popped value.
                 s.pop();
             }
             s.push(make_pair(heights[i], start));
