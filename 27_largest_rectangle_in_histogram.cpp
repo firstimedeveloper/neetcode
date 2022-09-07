e8d22100c254c7e8f9d408e83ab9e943096890ec
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-09-07 17:05:53 +0900

    solved using a monotonic stack

--- 27_largest_rectangle_in_histogram.cpp
+++ 27_largest_rectangle_in_histogram.cpp
@@ -4,17 +4,21 @@ public:
 		int ret = 0;
 		stack<pair<int, int>> s; // (height, pos)
 		for (int i=0; i<heights.size(); ++i) {
-			if (!s.empty() && heights[i] < s.top().first) {
-				int countPopped = 0;
-				while (!s.empty() && s.top().second > heights[i]) {
-					ret = max(ret, (i - s.top().second) * s.top().first);
-					s.pop();
-					countPopped++;
-				}
-				s.push(make_pair(heights[i], i - countPopped));
-			} else
-				s.push(make_pair(heights[i], i));
+            int start = i;
+            while (!s.empty() && heights[i] < s.top().first) {
+                printf("%d-%d x %d = %d\n", i, s.top().second, s.top().first, (i - s.top().second) * s.top().first);
+                ret = max(ret, (i - s.top().second) * s.top().first);
+                start = s.top().second;
+                s.pop();
+            }
+            s.push(make_pair(heights[i], start));
 		}
+        int i = heights.size();
+        while (!s.empty()) {
+            printf("%d-%d x %d = %d\n", i, s.top().second, s.top().first, (i - s.top().second) * s.top().first);
+            ret = max(ret, (i - s.top().second) * s.top().first);
+			s.pop();
+        }
         return ret;
     }
 };
