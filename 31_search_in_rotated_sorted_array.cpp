0671721f0319e3805f7389057386d8bec314b4de
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-09-10 23:45:35 +0900

    re-implementing to search in logn time complexity

--- 31_search_in_rotated_sorted_array.cpp
+++ 31_search_in_rotated_sorted_array.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    int search(vector<int>& nums, int target) {
+		int l = 0;
+		int r = nums.size();
+		while (l < r) {
+			int mid = l + (r - l) / 2;
+			if (target == nums[mid])
+				return mid;
+			if (nums[l] < nums[mid]) {
+				if (target > nums[mid])
+					l = mid + 1;
+				else if (target < nums[l])
+					l = mid + 1;
+				else
+					r = mid;
+			} else {
+				if (target < nums[mid])
+					r = mid;
+				else if (target > nums[r])
+					r = mid;
+				else
+					l = mid + 1;
+			}
+		}
+		return -1;
+    }
+};
+
+class Solution {
+public:
 	// Implement a binary search where left = 0, right is the size of the nums vector
 	// since we know the range of the nums is not changed, only the starting point.
 	// We only nee to find the starting point of the nums vector to be able to shift
