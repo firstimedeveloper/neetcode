dcaf1f62746d09844be1e636013e65d908173e38
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-09-11 17:46:37 +0900

    changed code so that while loop is when l <= r

--- 31_search_in_rotated_sorted_array.cpp
+++ 31_search_in_rotated_sorted_array.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
+	// binary search
+	// if L <= M
+	// 		if target > M or target < L
+	// 			search right (l = mid + 1)
+	// else
     int search(vector<int>& nums, int target) {
 		int l = 0;
-		int r = nums.size();
-		while (l < r) {
+		int r = nums.size() - 1;
+		while (l <= r) {
 			int mid = l + (r - l) / 2;
 			if (target == nums[mid])
 				return mid;
 			if (nums[l] <= nums[mid]) {
-				if (target > nums[mid])
-					l = mid + 1;
-				else if (target < nums[l])
+				if (target > nums[mid] || target < nums[l])
 					l = mid + 1;
 				else
-					r = mid;
+					r = mid - 1;
 			} else {
-				if (target < nums[mid])
-					r = mid;
-				else if (target > nums[r-1])
-					r = mid;
+				if (target < nums[mid] || target > nums[r])
+					r = mid - 1;
 				else
 					l = mid + 1;
 			}
