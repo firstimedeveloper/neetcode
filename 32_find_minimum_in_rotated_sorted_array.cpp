90ab6c912e0ef9c92f867935623775779cdf9933
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-09-13 00:21:07 +0900

    solved using a binary search

--- 32_find_minimum_in_rotated_sorted_array.cpp
+++ 32_find_minimum_in_rotated_sorted_array.cpp
@@ -1,27 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
     int findMin(vector<int>& nums) {
         int l = 0;
         int r = nums.size() -1;
+        int ret = nums[0];
         
         while (l <= r) {
             int mid = l + (r - l) / 2;
-			printf("%d %d %d\n", l, mid, r);
-            if (mid != 0 && nums[mid-1] > nums[mid])
-                return nums[mid];
+            printf("%d %d %d\n", nums[l], nums[mid], nums[r]);
+            if (nums[mid] < ret)
+                ret = nums[mid];
             if (nums[l] <= nums[mid]) {
                 if (nums[l] < nums[r])
                     r = mid - 1;
-                else
+                else 
                     l = mid + 1;
             } else {
-                r = mid;
-                
+                r = mid - 1;
             }
         }
-        return nums[l];
+        return ret;
     }
 };
 
