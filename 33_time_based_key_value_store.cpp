d10968696903fe9d8b06d0bd69839fb9029a0ca8
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-09-13 23:18:02 +0900

    changed to map of vectors of pairs, much more efficient.

--- 33_time_based_key_value_store.cpp
+++ 33_time_based_key_value_store.cpp
@@ -3,37 +3,27 @@ using namespace std;
 
 class TimeMap {
 public:
-    unordered_map<string, map<int, string>> store;
+    unordered_map<string, vector<pair<int, string>>> store;
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        store[key][timestamp] = value;
+        store[key].push_back(make_pair(timestamp, value));
     }
     
     string get(string key, int timestamp) {
         string ret = "";
 			
 		if (store.count(key)) {
-			unordered_map<string, map<int, string>>::iterator it = store.find(key);
-			map<int, string>::iterator i = (it->second).find(timestamp);
-			if (i != store[key].end())
-				ret = i->second;
-			else {
-				while (i != store[key].begin()) {
-					--i; // decrement first since begin is technically past the end and this also allows us to access the first element.
-					if (i->first <= timestamp) {
-						ret = i->second;
-						break ;
-					}
-				}
-			}
+            for (auto i=store[key].rbegin(); i != store[key].rend(); ++i) {
+                if (i->first <= timestamp)
+                    return i->second;
+            }   
 		}
         return ret;
     }
 };
-
 int main() {
 	string key = "foo";
 	string value = "bar";
