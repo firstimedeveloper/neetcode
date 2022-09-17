c836925716b5a9048390d0b683cf7e225f161627
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-09-18 00:05:48 +0900

    solved using a vector

--- 37_reorder_list.cpp
+++ 37_reorder_list.cpp
@@ -1,3 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	// ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 class Solution {
 public:
     void reorderList(ListNode* head) {
@@ -7,6 +18,7 @@ public:
 
 		while (head) {
 			lst.push_back(head);
+            head = head->next;
 		}
 
 		int len = lst.size();
@@ -14,15 +26,35 @@ public:
 		int r = len - 1;
 
         temp = ret;
-		while (l < r) {
-            cout << lst[l];
+		while (l <= r) {
+			printf("%d %d\n", l, lst[l]->val);
 			ret->next = lst[l++];
 			ret = ret->next;
-			if (l < r) break ;
-            cout << lst[r];
+			if (l > r) break ;
+            printf("%d %d\n", r, lst[r]->val);
 			ret->next = lst[r--];
 			ret = ret->next;
 		}
+		ret->next = NULL;
         head = temp->next;
     }
 };
+
+int main() {
+	ListNode *l1 = new ListNode(1);
+	ListNode *l2 = new ListNode(2);
+	ListNode *l3 = new ListNode(3);
+	ListNode *l4 = new ListNode(4);
+	l1->next = l2;
+	l2->next = l3;
+	l3->next = l4;
+
+	Solution sol;
+
+	sol.reorderList(l1);
+	while (l1) {
+		printf("%d ", l1->val);
+		l1 = l1->next;
+	}
+
+}
