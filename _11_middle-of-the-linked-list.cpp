991660bec8cd7ffa2d9e7f95dd53e96dc707bd20
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-09-29 23:30:50 +0900

    solved using two pointers

---
+++ _11_middle-of-the-linked-list.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *l = head;
+        ListNode *r = head;
+        for (;r && r->next; r=r->next->next) {
+            l = l->next;
+        }
+        return l;
+    }
+};
